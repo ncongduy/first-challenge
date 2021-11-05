b6ccb539c8aad493458e7049205c82dd65bf6db6
Nguyen Cong Duy <ncongduy@gmail.com>
2021-11-05 18:20:57 +0200

    create first slider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import MinimumDistanceSlider from './components/slider';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div>
+				<h2>i'm weekly available at</h2>
+				<p>
+					Note: Currently availability sliders are disabled. Coming
+					soon!
+				</p>
+
+				<MinimumDistanceSlider />
+
+				<Button
+					variant='contained'
+					onClick={() => {
+						alert('saved!');
+					}}
+				>
+					Save
+				</Button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/slider.js
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+
+function valuetext(value) {
+	return `${value}°C`;
+}
+
+const minDistance = 10;
+
+function createMarks() {
+	const marks = [];
+	const startHour = 6;
+	const endHour = 23;
+	const labels = [2, 6, 10, 14];
+
+	const range = (endHour - startHour) * 10;
+
+	for (let i = 0; i <= range; i = i + 10) {
+		const mark = {};
+		mark['value'] = i;
+		marks.push(mark);
+	}
+
+	marks.forEach((mark, index) => {
+		if (labels.includes(index)) {
+			mark['label'] = startHour + index;
+		} else {
+			mark['label'] = '|';
+		}
+	});
+
+	return marks;
+}
+
+export default function MinimumDistanceSlider() {
+	const [value1, setValue1] = React.useState([100, 140]);
+
+	const handleChange1 = (event, newValue, activeThumb) => {
+		if (!Array.isArray(newValue)) {
+			return;
+		}
+
+		if (activeThumb === 0) {
+			setValue1([
+				Math.min(newValue[0], value1[1] - minDistance),
+				value1[1],
+			]);
+		} else {
+			setValue1([
+				value1[0],
+				Math.max(newValue[1], value1[0] + minDistance),
+			]);
+		}
+	};
+
+	return (
+		<Box sx={{ width: 800 }}>
+			<Slider
+				getAriaLabel={() => 'Minimum distance'}
+				min={0}
+				max={170}
+				step={10}
+				value={value1}
+				onChange={handleChange1}
+				marks={createMarks()}
+				// valueLabelDisplay='auto'
+				getAriaValueText={valuetext}
+				disableSwap
+			/>
+			<Box sx={{ m: 3 }} />
+		</Box>
+	);
+}
